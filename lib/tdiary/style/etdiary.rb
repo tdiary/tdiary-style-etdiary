f42e806992b6360deb0b437c4a729b3c1e6497e3
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-08-27 19:59:12 +0900

    bundle gem tdiary-style-etdiary -t

---
+++ lib/tdiary/style/etdiary.rb
@@ -0,0 +1,9 @@
+require "tdiary/style/etdiary/version"
+
+module Tdiary
+  module Style
+    module Etdiary
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tdiary/style/etdiary/version.rb
@@ -0,0 +1,7 @@
+module Tdiary
+  module Style
+    module Etdiary
+      VERSION = "0.0.1"
+    end
+  end
+end
